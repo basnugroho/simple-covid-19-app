4fbdb9dac071d4eb720a83a10215cf8b9f24bb2d
Baskoro Nugroho <bas.nugroho@yahoo.com>
2020-09-27 13:11:16 +0700

    Initial Commit

---
+++ Codiv-19 Swift UI API/Codiv_19_Swift_UI_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codiv_19_Swift_UI_APIApp.swift
+//  Codiv-19 Swift UI API
+//
+//  Created by Baskoro Nugroho on 27/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Codiv_19_Swift_UI_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Codiv-19 Swift UI API/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Codiv-19 Swift UI API
+//
+//  Created by Baskoro Nugroho on 27/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
